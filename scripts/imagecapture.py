8de08cc3a5316738884633cf35d066aaeb7c0902
SageSG <houliangyip@gmail.com>
2022-06-20 13:32:29 +0800

    Fix segmentation fault
    uncommented testing code

--- scripts/imagecapture.py
+++ scripts/imagecapture.py
@@ -227,28 +227,28 @@ def CaptureImagesOnVideo(videos_to_be_processed):
 
                         # cv2.waitKey(0)
 
-                            img1 = cv2.imread('template/1segment.png',1)          # queryImage
+                            # img1 = cv2.imread('template/1segment.png',1)          # queryImage
 
-                            # Initiate SIFT detector
-                            orb = cv2.ORB()
+                            # # Initiate SIFT detector
+                            # orb = cv2.ORB()
 
-                            # find the keypoints and descriptors with SIFT
-                            kp1, des1 = orb.detectAndCompute(img1,None)
-                            kp2, des2 = orb.detectAndCompute(saveCopy,None)
+                            # # find the keypoints and descriptors with SIFT
+                            # kp1, des1 = orb.detectAndCompute(img1,None)
+                            # kp2, des2 = orb.detectAndCompute(saveCopy,None)
                                 
-                            # create BFMatcher object
-                            bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+                            # # create BFMatcher object
+                            # bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
 
-                            # Match descriptors.
-                            matches = bf.match(des1,des2)
+                            # # Match descriptors.
+                            # matches = bf.match(des1,des2)
 
-                            # Sort them in the order of their distance.
-                            matches = sorted(matches, key = lambda x:x.distance)
+                            # # Sort them in the order of their distance.
+                            # matches = sorted(matches, key = lambda x:x.distance)
 
-                            # Draw first 10 matches.
-                            img3 = cv2.drawMatches(img1,kp1,saveCopy,kp2,matches[:10], flags=2)
+                            # # Draw first 10 matches.
+                            # img3 = cv2.drawMatches(img1,kp1,saveCopy,kp2,matches[:10], flags=2)
 
-                            plt.imshow(img3),plt.show()
+                            # plt.imshow(img3),plt.show()
                         
 
                         ###
