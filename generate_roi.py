4e67d5085ec1c2f9e4005900d3500aa95570b83e
yujing <yujing1314276@gmail.com>
2022-06-20 16:14:14 +0800

    add images

--- generate_roi.py
+++ generate_roi.py
@@ -1,11 +1,12 @@
 import cv2
 import argparse
 import re
+import os
 
 outline_thickness = 5
-path = "preprocess_images/new7.jpg"
-def get_roi():
-    img_color = cv2.imread(path)
+path = "preprocess_images2/"
+def get_roi(image):
+    img_color = cv2.imread(path+image)
     img_color = cv2.resize(img_color, None, None, fx=0.5, fy=0.5)
     img_color = cv2.rotate(img_color,cv2.ROTATE_90_COUNTERCLOCKWISE) #change orientation
     img = cv2.cvtColor(img_color, cv2.COLOR_BGR2GRAY)
@@ -30,10 +31,16 @@ def get_roi():
     roi = img[y_coor : y_coor + height, x_coor : x_coor + width] #crop image (roi)
     # cv2.imshow("ROI", roi) 
     # cv2.waitKey(0)
-    roi = cv2.rotate(roi,cv2.ROTATE_90_CLOCKWISE) #change orientation
+    # roi = cv2.rotate(roi,cv2.ROTATE_90_CLOCKWISE) #change orientation
 
-    img_name = re.search("(?<=\/)(.*)(?=\.jpg)",path).group()
-    cv2.imwrite(f"processed_images/{img_name}-roi.jpg", roi)
+    # img_name = re.search("(?<=\/)(.*)(?=\.jpg)",path).group()
+    cv2.imwrite(f"processed_images2/{image}-roi.jpg", roi)
     return roi
 
-get_roi()
+#function to validate if the item is an image
+def imageValidator():
+    for item in os.listdir(path):
+        # print(item)
+        get_roi(item)
+
+imageValidator()
