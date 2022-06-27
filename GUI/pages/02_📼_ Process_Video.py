17b672f29bae5f0a30a9979d931c5ed2ff8d4fea
Nicholas <nbingei@gmail.com>
2022-06-28 02:20:38 +0800

    Updates to GUI pages

--- GUI/pages/02_📼_ Process_Video.py
+++ GUI/pages/02_📼_ Process_Video.py
@@ -1,21 +1,62 @@
 import streamlit as st
+import pandas as pd
+from io import StringIO
+import numpy as np
 
+# Page Configs
 st.set_page_config(
     page_title="Process Video",
     page_icon="📼",
 )
 
-st.write('# Process Video')
-
+# Page Sidebar
 st.sidebar.success("Select a demo above.")
+
+# Page Title
+st.write('# 📼 Process Video 📼')
+
+# Line break
+st.write('###')
+
+st.markdown("""
+### There are 2 methods of processing the video.
+1. Processing the videos locally (Recommended)
+2. Processing the videos online via server
+""")
+
+st.write('###')
+
+st.markdown('### Upload video to start processing')
+
+uploaded_video = st.file_uploader("Choose a file")
+if uploaded_video is not None:
+    # To read file as bytes:
+    bytes_data = uploaded_video.getvalue()
+    st.write(bytes_data)
+
+    # To convert to a string based IO:
+    stringio = StringIO(uploaded_video.getvalue().decode("utf-8"))
+    st.write(stringio)
+
+    # To read file as string:
+    string_data = stringio.read()
+    st.write(string_data)
+
+    # Can be used wherever a "file-like" object is accepted:
+    dataframe = pd.read_csv(uploaded_video)
+    st.write(dataframe)
+
+st.video(uploaded_video)
+
+st.write('###')
+
+# TODO: Integrate imagecapture.py to Streamlit
 
 st.markdown(
     """
 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard 
 dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen 
-book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially 
-unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
-and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+book.
 """
 )
 
