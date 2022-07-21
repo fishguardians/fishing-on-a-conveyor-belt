2899d8d63e1c57ec81991d0f6de722cc57a41be4
SageSG <houliangyip@gmail.com>
2022-07-21 16:58:39 +0800

    unit testing

--- scripts/fish_measurement.py
+++ scripts/fish_measurement.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-'''_fish_measurement.py: Starts the fish measur
+'''_fish_measurement.py: Starts the fish measurement
     @Author: "Nicholas Bingei"
     @Credit: ["Muhammad Abdurraheem", "Chen Dong", "Nicholas Bingei", "Yao Yujing", "Yip Hou Liang"]'''
 
