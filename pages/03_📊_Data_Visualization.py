8f071f0642f47337041bb6f0f870e0d1e56acb2c
SageSG <houliangyip@gmail.com>
2022-07-30 16:46:34 +0800

    credits

--- pages/03_📊_Data_Visualization.py
+++ pages/03_📊_Data_Visualization.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''main.py: Main module that runs the fish phenotyping process
+    @Author: "Muhammad Abdurraheem and Nicholas Bingei"
+    @Credit: ["Muhammad Abdurraheem", "Chen Dong", "Nicholas Bingei", "Yao Yujing", "Yip Hou Liang"]'''
+# import if necessary (built-in, third-party, path, own modules)
 import os
 import streamlit as st
 import pandas as pd
